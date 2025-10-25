c8049a10f136b6871c25acc56157f2ed8d4eaead
MuhammadUzairChoudhary <uzairch3007@gmail.com>
2025-10-25 12:18:10 +0500

    changed QuotePage.jsx

--- src/components/QuotePage.jsx
+++ src/components/QuotePage.jsx
@@ -7,7 +7,7 @@ import SignatureCanvas from "./SignatureCanvas";
 import DatePicker from "./DatePicker";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://localhost:4000/api/",
+  baseURL: import.meta.env.VITE_API_URL || "https://looksbyanum-saqib.vercel.app/api/",
 });
 
 const CustomDatePicker = ({ label, name, register, error, required, maxDate }) => {
